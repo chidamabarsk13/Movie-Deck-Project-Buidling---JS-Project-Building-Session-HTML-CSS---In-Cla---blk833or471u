30d93cd71c4b7536efe31e5395dbe20727b402b1
chidamabarsk13 <142134688+chidamabarsk13@users.noreply.github.com>
2023-08-13 13:03:47 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,26 @@
+// Define variables and constants
+const apiKey = 'YOUR_API_KEY';
+const movieList = document.querySelector('.movie-list');
+const searchInput = document.getElementById('searchInput');
+const searchButton = document.getElementById('searchButton');
+
+// Fetch movies from the API and render them
+async function fetchMovies() {
+  // Fetch API data and populate the movieList
+
+}
+
+// Render movie cards
+function renderMovies(movies) {
+  // Create movie cards and append them to movieList
+}
+
+// Handle search button click
+searchButton.addEventListener('click', () => {
+  const searchTerm = searchInput.value;
+  // Perform search and update movieList
+  // ...
+});
+
+fetchMovies();
 
